ecf1a8362fb4dee12f5006645b28dcab5078bb40
Nirmal <nirmalkumarvs82@gmail.com>
2022-05-23 15:28:54 +0530

    Created Board class having only the constructor

--- src/Models/Board.py
+++ src/Models/Board.py
@@ -1,2 +1,9 @@
 class Board:
-    pass
+    rowLength = 0
+    columnLength = 0
+    BOARD_STARTING_INDEX = 0
+
+    def __init__(self, rowLength, columnLength):
+        self.board = [[0] * columnLength for row in range(rowLength)]
+        self.rowLength = rowLength
+        self.columnLength = columnLength

558338e722dc7415f337100144d56c11648eeb4f
Nirmal <nirmalkumarvs82@gmail.com>
2022-05-23 16:17:08 +0530

    tested makeItAsActiveCell method

--- src/Test.py
+++ src/Test.py
@@ -1,12 +1,23 @@
 # Python code to demonstrate working of unittest
 import unittest
-from Models import Board
-
+from src.Models.Board import Board
+from src.Controller.GameOfLife import GameOfLife
 class TestMethods(unittest.TestCase):
 
     def test_Board(self):
-        self.assertEqual([[0]*50 for i in range(10)],Board.Board(10,50).board)
+        self.assertEqual([[0]*50 for i in range(10)],Board(10,50).board)
 
+    def test_makeItASActiveCell_method(self):
+        boardObj1=Board(10,50)
+        boardObj2=Board(10,50)
+        gameOfLifeObj=GameOfLife()
+        for i in range(5):
+            boardObj1.board[i][i]=1
+            gameOfLifeObj.makeItASActiveCell(boardObj2.board,i,i)
+
+        self.assertEqual(boardObj2.board,boardObj1.board)
+
+    
 
 if __name__ == '__main__':
     unittest.main()

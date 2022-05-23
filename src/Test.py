9c3499bc3dc830cf6c69f23cade36f292e357976
Nirmal <nirmalkumarvs82@gmail.com>
2022-05-23 15:32:23 +0530

    tested the Board and test came successful

--- src/Test.py
+++ src/Test.py
@@ -5,7 +5,7 @@ from Models import Board
 class TestMethods(unittest.TestCase):
 
     def test_Board(self):
-        self.assertEqual([[0]*50 for i in range(10)],Board(10,50).board)
+        self.assertEqual([[0]*50 for i in range(10)],Board.Board(10,50).board)
 
 
 if __name__ == '__main__':
